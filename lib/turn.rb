2e78cbe32e1d1223ad265fe017ab7a301cb46207
affable-motherboard-2531 <solomonrfeldman@gmail.com>
2019-03-20 15:26:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,12 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   index = gets.strip
+  index = input_to_index(index)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+    turn(board)
+  end
 end
 
 def input_to_index(index)
