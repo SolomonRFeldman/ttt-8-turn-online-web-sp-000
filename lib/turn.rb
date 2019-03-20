2a266498405e3e6b490fb33946ee89fd06f548e9
affable-motherboard-2531 <solomonrfeldman@gmail.com>
2019-03-20 15:17:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,24 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(index)
+  index = index.to_i
+  index = index - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
 def position_taken?(board, index)
 # !(board[index] == ""  || 
 # board[index] == " " || 
